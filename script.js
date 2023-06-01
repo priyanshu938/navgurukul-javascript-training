69d39099f02db673f2e705c97434cc6c66d9add4
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-06-01 22:36:47 +0530

    variables completed

---
+++ script.js
@@ -0,0 +1,26 @@
+let a; //declaration
+a = 35; //assignment/ initialization
+console.log(a);
+a = 34; //can be re-assign
+
+const b = 34; // const declarations must be initialized, const cannot be re-assigned
+
+console.log(b);
+
+// let and const cannot be re-declared
+var c = 5; // var can be re-declared
+var c = 8;
+console.log(c);
+
+/* => multiline comment
+hello javascript
+*/
+
+const d = 5,
+  e = 6,
+  f = 7;
+console.log(d, e, f);
+
+console.log("hello world\njs", d);
+
+// Naming Convention in JS
