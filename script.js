04bd46685cb7aa12e9be2cb588d64c81b3898a06
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-06-05 22:53:46 +0530

    ternary operator and && operator

--- script.js
+++ script.js
@@ -32,3 +32,22 @@ switch (day) {
 }
 
 // execution of more than case in absence of break keyword is called fall through
+
+//ternary operator
+let age = 18;
+let x = "name";
+// if else
+if (age >= 18) {
+  console.log("yes");
+} else {
+  console.log("no");
+}
+// ternary operator => if...else alternative
+console.log(age >= 18 ? "yes" : "no");
+
+// if part
+if (age >= 18) {
+  console.log("yes");
+}
+// && operator => if alternative
+console.log(age >= 18 && "yes");
