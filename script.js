f5850a2a4abafaa5c0506bc84e48db8150769b33
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-06-05 21:41:15 +0530

    if else completed

---
+++ script.js
@@ -0,0 +1,18 @@
+//if and if-else
+let a = 2;
+let b = 4;
+if (a > b) {
+  console.log("a is greater than b");
+} else {
+  console.log("a is less than b");
+}
+// if..else if...else
+let c = 2;
+let d = 4;
+if (c > d) {
+  console.log("c is greater than d");
+} else if (c == d) {
+  console.log("c is equal to d");
+} else {
+  console.log("c is less than d");
+}
