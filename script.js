f2b072f9c827208476e181660e92cf6c5182268c
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-06-08 21:20:10 +0530

    math functions completed

---
+++ script.js
@@ -0,0 +1,39 @@
+//Math.PI => pi constant
+const radius = 3;
+const area = Math.PI * radius * radius;
+console.log(area);
+
+//Math.round() => round to nearest integer
+console.log(Math.round(2.5));
+
+//Math.floor() => round down to nearest integer
+console.log(Math.floor(4.7)); //4 < 4.7 < 5
+
+//Math.ceil() => round up to nearest integer
+console.log(Math.ceil(4.7)); //4 < 4.7 < 5
+
+//Math.trunc() => remove decimal part
+console.log(Math.trunc(4.7)); //4
+
+//Math.pow() => power
+console.log(Math.pow(2, 3)); //2^3, 2**3
+
+//Math.sqrt() => square root
+console.log(Math.sqrt(16)); //4
+
+//Math.abs() => absolute value
+console.log(Math.abs(-4)); //4
+
+//Math.max() => maximum value
+console.log(Math.max(2, 3, 4, 5)); //5
+
+//Math.min() => minimum value
+console.log(Math.min(2, 3, 4, 5)); //2
+
+//Math.random() => random number between 0 and 1
+console.log(Math.random()); //0.123456789
+
+//random number between 0 and 10
+console.log(Math.random() * 10); //0.123456789
+
+// Math.sin(), Math.cos(), Math.tan(), Math.log(),  => trigonometry
