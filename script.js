6951348953d18dffe85f21162dff76cd1fc65b34
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-06-09 22:11:21 +0530

    completed

---
+++ script.js
@@ -0,0 +1,10 @@
+//alert
+alert("Hello, World!");
+
+//prompt
+let x = prompt("What is your name?");
+console.log(x);
+
+//confirm
+let y = confirm("Are you sure you want to delete this post?");
+console.log(y);
