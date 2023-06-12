ce124fd5dcab640ccfa1768d2fcd6c73663a7969
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-06-12 22:09:57 +0530

    function intro completed

---
+++ functions.js
@@ -0,0 +1,53 @@
+console.log("hello");
+let a = 3;
+// function declaration/statement
+function helloFunction() {
+  //function body
+  console.log("hello from functions");
+}
+
+helloFunction(); //function invocation / function call
+
+// function expression
+//Anonymous function expression => without any name
+const helloFunctionExpressionAnonymous = function () {
+  console.log("hello from function expression anonymous");
+};
+helloFunctionExpressionAnonymous();
+console.log(helloFunctionExpressionAnonymous);
+
+// Named function expression
+const helloFunctionExpressionNamed = function xyz() {
+  console.log("hello from function expression named");
+};
+helloFunctionExpressionNamed();
+console.log(helloFunctionExpressionNamed);
+// xyz(); => not allowed
+
+//IIFEs => Immediately invoked function expression
+(function () {
+  console.log("hello from IIFES");
+})();
+
+//Parameters and arguments
+function sum(a, b) {
+  // Parameters => values passed at time of function declaration
+  return a + b;
+}
+let sumValue = sum(2, 3); // Arguments=> values passed at time of calling the function
+console.log(sumValue);
+
+function sumParent(a, b) {
+  //nested functions
+
+  function sumChild(c, d, func) {
+    console.log(func());
+    return c + d;
+  }
+  console.log(sumChild);
+  return sumChild(3, 3, function () {
+    return 4;
+  });
+}
+
+console.log(sumParent(2, 3));
