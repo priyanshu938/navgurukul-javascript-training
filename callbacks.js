04c18bf48e5708689710f6cba7f6706dfdbe4006
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-06-13 21:20:22 +0530

    callbacks

---
+++ callbacks.js
@@ -0,0 +1,3 @@
+//A callback function is a function passed into another function as an argument.
+/*Higher-order functions in JavaScript are functions that operate on other functions,
+either by taking them as arguments or by returning them. */
