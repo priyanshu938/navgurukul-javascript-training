ce0f74e24ac7b923aa67ed6b8faeb02e3c8a17b5
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-06-13 21:50:48 +0530

    +1

---
+++ timing_functions.js
@@ -0,0 +1,10 @@
+//seTimeout : this method executes a function, after waiting a specified number of milliseconds.
+//setInterval : this method repeats a given function at every given time-interval.
+
+setTimeout(() => {
+  alert("Hello setTimeout");
+}, 3000);
+
+setInterval(() => {
+  alert("Hello setInterval");
+}, 3000);
