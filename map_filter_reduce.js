8c3e94c2bd4d238bd4f2668f2cd3d1c424179abb
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-06-15 21:24:27 +0530

    reduce done

--- map_filter_reduce.js
+++ map_filter_reduce.js
@@ -17,3 +17,18 @@ console.log(outputMap);
 //filter method
 const outputFilter = arr.filter((x) => x % 2 === 0); //for even number
 console.log(outputFilter);
+
+//sum of array elements
+const arr1 = [5, 1, 30, 2, 6, 21, 18];
+const sum = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i]; //5,6,
+  }
+  return sum;
+};
+console.log(sum(arr1));
+
+//reduce method
+const outputReduce = arr.reduce((acc, curr) => acc + curr, 0);
+console.log(outputReduce);
