a7f5666f85e2248fcc29a20949f940c297d1fd35
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-06-16 22:44:33 +0530

    shadowing completed

--- script.js
+++ script.js
@@ -16,18 +16,39 @@
 // console.log(c);
 
 //function scope
-var a = 10;
-let b = 20;
-const c = 30;
-// var a=9;
-// console.log(a)
-function abc() {
-  a = 100;
-  b = 200;
-  x = 1000; //works as a global variable
-  const c = 300;
-  console.log(a, b, c, x);
-  //lexical environment => inner function can access outer function variable
+// var a = 10;
+// let b = 20;
+// const c = 30;
+// // var a=9;
+// // console.log(a)
+// function abc() {
+//   a = 100;
+//   b = 200;
+//   x = 1000; //works as a global variable
+//   const c = 300;
+//   console.log(a, b, c, x);
+//   //lexical environment => inner function can access outer function variable
+// }
+// abc();
+// console.log(a, b, c, x);
+
+//shadowing
+var a = 1000;
+{
+  var a = 10;
+  let b = 20;
+  const c = 30;
+  {
+    let a = -10;
+    let b = -20;
+    console.log(a, b); //-10,-20,-30
+  }
+  console.log(a, b, c); //10,20,30
 }
-abc();
-console.log(a, b, c, x);
+console.log(a); //10
+
+//illegal shadowing
+// let  m = 1000;
+// {
+//     var m=10;
+// }
