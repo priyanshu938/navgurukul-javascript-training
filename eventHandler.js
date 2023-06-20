9c96972b3da267213b22d5920c5b393e192ee6c7
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-06-20 21:53:28 +0530

    event listener

---
+++ eventHandler.js
@@ -0,0 +1,13 @@
+function myFunction() {
+  console.log("box 1 called");
+}
+
+function mouseOver() {
+  console.log("mouse over");
+}
+
+function keyPress(e) {
+  console.log(e);
+  //console.log(event.key); //tracks the key pressed
+  // console.log(event.target.value); //tracks the value of the key pressed
+}
