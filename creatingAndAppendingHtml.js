71d8dfe421e338d1641379fd5df2b462cf564c3c
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-06-20 21:10:30 +0530

    insertAdjacentElements

--- creatingAndAppendingHtml.js
+++ creatingAndAppendingHtml.js
@@ -3,8 +3,19 @@ newPara.textContent = "This is a new paragraph";
 //<p>This is a new paragraph</p>
 
 const container = document.getElementById("container");
-container.appendChild(newPara);
+// container.appendChild(newPara);
 
 const button = document.createElement("button");
 button.innerHTML = "submit";
-container.appendChild(button);
+// container.appendChild(button);
+
+//inserting html
+const divBox = `
+<div class="box" id="box-5">box-5</div>
+<div class="box" id="box-6">box-6</div>
+`;
+
+container.insertAdjacentHTML("beforeend", divBox);
+// container.insertAdjacentHTML("beforebegin", divBox);
+// container.insertAdjacentHTML("afterbegin", divBox);
+// container.insertAdjacentHTML("afterend", divBox);
