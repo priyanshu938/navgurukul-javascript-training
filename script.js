1f21af5bb5585201001b7f285850c72409c6234e
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-06-22 21:58:13 +0530

    +1

---
+++ script.js
@@ -0,0 +1,57 @@
+let arrayOfDrums = document.querySelectorAll(".drum");
+
+for (let i = 0; i < arrayOfDrums.length; i++) {
+  arrayOfDrums[i].addEventListener("click", function () {
+    let buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+  arrayOfDrums[i].addEventListener("keydown", function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+  });
+}
+
+const makeSound = (key) => {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    default:
+      alert("Wrong key pressed");
+  }
+};
+
+const buttonAnimation = (key) => {
+  let activeButton = document.querySelector(`.${key}`);
+  activeButton.classList.add("pressed");
+
+  setTimeout(() => {
+    activeButton.classList.remove("pressed");
+  }, 100);
+};
