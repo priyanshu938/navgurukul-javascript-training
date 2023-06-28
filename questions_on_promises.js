18ef06757f68821092b2c8109d4619fd137e0d38
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-06-28 15:28:22 +0530

    +1

---
+++ questions_on_promises.js
@@ -0,0 +1,19 @@
+//Create a function that takes an array of numbers and returns a promise that resolves to the sum of all numbers.
+
+function sumArray(numArray) {
+  return new Promise((res, rej) => {
+    const sum = numArray.reduce((acc, curr) => acc + curr, 0);
+    res(sum);
+  });
+}
+
+const wrapperFunction = async () => {
+  try {
+    const numbers = [1, 2, 3, 4, 5];
+    let sum = await sumArray(numbers);
+    console.log(sum);
+  } catch (error) {
+    console.log(error);
+  }
+};
+wrapperFunction();
